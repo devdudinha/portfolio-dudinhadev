9960d9f4c587254b7a44e7f92424ffdafd100c4a
Maria Eduarda <151252340+devdudinha@users.noreply.github.com>
2025-02-23 22:36:25 -0300

    Update index.js

---
+++ index.js
@@ -0,0 +1,4 @@
+document.getElementById('menu-toggle').addEventListener('click', function() {
+    var menuLateral = document.getElementById('menu-lateral');
+    menuLateral.classList.toggle('active');
+});
